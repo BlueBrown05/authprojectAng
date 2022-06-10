4bb2f8d867db9e25cbba3e30f922ae0b512cd76d
BlueBrown05 <1970005@kiit.ac.in>
2022-06-10 14:23:00 +0530

    first commit

---
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -0,0 +1,109 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Forgotpassword } from 'src/app/models/forgotpassword.model';
+import { ApiServiceService } from 'src/app/services/api-service.service';
+import { NotificationService } from 'src/app/services/notification.service';
+
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent implements OnInit {
+  securityQuestion!: any;
+  forgetPasswordForm!: FormGroup;
+  showMainForm: boolean = true;
+  showPassForm: boolean = false;
+  email: any;
+
+  forgetPasswordModel: Forgotpassword = new Forgotpassword();
+
+  constructor(private authService: ApiServiceService, private fb: FormBuilder, private router: Router, private notifyService: NotificationService) { }
+
+  ngOnInit(): void {
+    this.getSecurityQuestions();
+    this.CreateforgetPasswordForm();
+  }
+
+
+  CreateforgetPasswordForm() {
+    this.forgetPasswordForm = this.fb.group(
+      {
+
+        Email: ['', Validators.required],
+        Password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(8)]],
+        ConfirmPassword: ['', Validators.required],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('Password')?.value === g.get('ConfirmPassword')?.value ? null : { mismatch: true };
+  }
+
+  getSecurityQuestions() {
+    this.authService.getSecurityQuestions().subscribe(data => {
+      this.securityQuestion = [];
+
+      this.securityQuestion = data.security;
+
+    }, err => {
+      console.log(err);
+    })
+  }
+
+  verifyEmailWithSecurity() {
+
+
+    let email = (document.getElementById("email") as HTMLInputElement).value;
+    let question = (document.getElementById("security") as HTMLInputElement).value;
+    let ans = (document.getElementById("ans") as HTMLInputElement).value;;
+    this.email = email;
+
+    this.authService.verifyUserWithSecurity(email, question, ans).subscribe(data => {
+      console.log(data);
+      if (data == 0) {
+        this.notifyService.showError("Credential Not Matching..", "Failed to varify");
+        this.router.navigate(['/login']);
+      } else {
+        this.notifyService.showSuccess("You can reset your password", "Matched");
+
+        this.showMainForm = false;
+        this.showPassForm = true;
+
+      }
+
+
+    }, err => {
+      console.log(err);
+    })
+  }
+
+  resetPassword() {
+    if (this.forgetPasswordForm.valid) {
+      this.forgetPasswordModel.Email = this.email;
+      this.forgetPasswordModel.Password = this.forgetPasswordForm.value.Password;
+
+
+
+      this.authService.resetPassword(this.forgetPasswordModel).subscribe(
+        data => {
+          this.notifyService.showSuccess("Password reset successfully done", "Password reset");
+
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          this.notifyService.showError("Failed to reset password", error.error);;
+        }
+      );
+    } else {
+      // console.log("invalid");
+      this.notifyService.showError("Reset Failed", "Fill the required field correctly");
+    }
+  }
+
+
+}
---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,129 @@
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { ApiServiceService } from 'src/app/services/api-service.service';
+import { NotificationService } from 'src/app/services/notification.service';
+
+
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  dob: any;
+  gender: any;
+  fullName: any;
+  mobile: any;
+  email: any;
+  securityQuestion: any;
+  securityAns: any;
+
+  profileForm!: FormGroup;
+  userModel: User = new User();
+
+  constructor(private authService: ApiServiceService, private fb: FormBuilder, private router: Router, private notifyService: NotificationService, private datepipe: DatePipe) { }
+
+
+
+  ngOnInit(): void {
+    this.getProfileData(localStorage.getItem('userID'));
+    this.CreateProfileForm();
+
+
+  }
+  CreateProfileForm() {
+    this.profileForm = this.fb.group(
+      {
+        UserID: ['', Validators.required],
+
+        Gender: ['0', Validators.required],
+        Mobile: ['', Validators.required],
+        DOB: ['', Validators.required],
+        FullName: ['', Validators.required],
+
+      }
+
+    );
+  }
+
+
+
+
+  getProfileData(id: any) {
+    this.authService.getProfileData(id).subscribe(data => {
+      const user = data.profile[0];
+      console.log(user)
+      this.dob = user.dob == "1900-01-01T00:00:00" ? "Not Available" : this.datepipe.transform(user.dob, 'dd/MM/yyyy');
+      this.gender = user.gender == "" ? "Not Available" : user.gender;
+      this.fullName = user.fullName;
+      this.mobile = user.mobile == "" ? "Not Available" : user.mobile;
+      this.email = user.email;
+      this.securityQuestion = user.securityQuestion;
+      this.securityAns = user.securityAns;
+
+    }, err => {
+      console.log(err);
+    })
+  }
+
+  onEdit() {
+    console.log(this.dob)
+    let date = this.dob == "Not Available" ? "" : this.datepipe.transform(this.dob, 'yyyy-MM-dd');
+    console.log(date);
+
+    this.profileForm = this.fb.group({
+
+
+      UserID: [localStorage.getItem('userID'), Validators.required],
+      Gender: [this.gender == "Not Available" ? "0" : this.gender, Validators.required],
+      Mobile: [this.mobile == "Not Available" ? "" : this.mobile, Validators.required],
+      FullName: [this.fullName, Validators.required],
+
+      DOB: [date, Validators.required],
+
+    });
+
+
+
+  }
+
+  profileUpdate() {
+    if (this.profileForm.valid && this.profileForm.value.Gender != '0') {
+
+      var DDate = (document.getElementById("date") as HTMLInputElement).value;
+      console.log(DDate);
+
+      this.profileForm.value.DOB = DDate;
+
+
+      this.userModel.FullName = this.profileForm.value.FullName;
+      this.userModel.Gender = this.profileForm.value.Gender;
+      this.userModel.Mobile = this.profileForm.value.Mobile;
+      this.userModel.DOB = this.profileForm.value.DOB;
+      this.userModel.UserID = this.profileForm.value.UserID;
+
+      console.log(this.userModel);
+
+      this.authService.register(this.userModel).subscribe(
+        data => {
+          this.notifyService.showSuccess("Profile Updated", "Update");
+          let ref = document.getElementById('cancel');
+          ref?.click();
+
+          this.getProfileData(localStorage.getItem('userID'));
+        },
+        (error) => {
+          this.notifyService.showError("Profile Update Failed", error.error);;
+        }
+      );
+    } else {
+      this.notifyService.showError("Profile Update Failed", "Fill the required field correctly");
+    }
+  }
+
+}
